41f22b0cb3c2c062fb980a2a6b91cf52ce3ee6b5
Cryzepy <91277901+Cryzepy@users.noreply.github.com>
2023-03-20 00:22:04 +0700

    Add files via upload
    first upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// inisialisasi AOS
+AOS.init({
+	offest: 0
+})
+
+// Random time animation skills logo
+const skillsLogo = document.querySelectorAll('.skills-logo')
+
+.forEach(logo => {
+	logo.style.animationDuration = `${~~(Math.random() * 3000 + 800)}ms`
+})
