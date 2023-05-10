20dec8569e8be492ddc18ae57f19ea50294b3536
fikrialfian <91277901+Cryzepy@users.noreply.github.com>
2023-05-10 22:16:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ AOS.init({
 })
 
 // Random time animation skills logo
-const skillsLogo = document.querySelectorAll('.skills-logo')
-
-.forEach(logo => {
+const skillsLogo = document.querySelectorAll('.skills-logo').forEach(logo => {
 	logo.style.animationDuration = `${~~(Math.random() * 3000 + 800)}ms`
-});
+})
